a1fbcc4a3e072c16dac9364000937de2613988c0
John Michael <ducnm.john98@gmail.com>
2018-05-07 19:19:10 +0700

    d

--- routes/update.js
+++ routes/update.js
@@ -3,22 +3,24 @@ var router = express.Router();
 var sequelize = require('../config/sequelize');
 var Sequelize = require('sequelize');
 
-router.post('/', (req, res, next) => {
-  console.log(req.body.temperatureOut);
-  console.log(req.body.temperatureIn);
-  console.log(req.body.humidity);
+router.get('/:humidityOut/:humidityIn/:temperature', (req, res, next) => {
+  console.log(req.params.temperatureOut);
+  console.log(req.params.temperatureIn);
+  console.log(req.params.humidity);
   sequelize.query("CALL `insertAgriculterControl`(:potID, :humidityOut, :humidityIn, :temperature)", {
     replacements: {
       potID: 1,
-      humidityOut: req.body.humidityOut,
-      humidityIn: req.body.humidityIn,
-      temperature: req.body.temperature,
+      humidityOut: req.params.humidityOut,
+      humidityIn: req.params.humidityIn,
+      temperature: req.params.temperature,
     }
   }).then(data => {
     console.log(data, '\n Data send to server at ', new Date());
+    res.send("Update successful")
     next();
   }).catch(err => {
     console.log('Data send to server at ', new Date(), ' has an errorr: ', err);
+    res.send("Update erros")
     next();
   })
 });
